7f3d47fbc02d867f504d86d9e9619d8d10bb7780
AdhamMoSaleh <adham.work24@gmail.com>
2026-05-12 09:53:02 +0300

    Add local end vars and update db password

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /** Lets you open dev from another device on LAN without Next blocking HMR. */
+  allowedDevOrigins: ["192.168.1.2"],
 };
 
 export default nextConfig;
